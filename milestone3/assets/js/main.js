642c6a169c88477482f7fa931da4fc9fd309b444
alessio <alessiodg99.adg@gmail.com>
2020-12-11 18:00:56 +0100

    aggiunto constructor

--- milestone3/assets/js/main.js
+++ milestone3/assets/js/main.js
@@ -1,123 +1,77 @@
 $(document).ready(function () {
-    // Qui le icone che abbiamo definito nella milestone 1
-    
-
-    // definiamo dei colori per le icone (blue, orange, purple)
-    
-    //aggiungiamo dei colori usando una funzione
-    
-    //inseriamo le icone colorate nel container
-    
-  
-  });
-  
-$(document).ready(function () {
     // icone come da milestone 1
-    let icone = [
-        {
-            name: "cat",
-            prefix: "fas",
-            type: "fa",
-            family: "cat"
-        },
-
-        {
-            name: "crow",
-            prefix: "fas",
-            type: "fa",
-            family: "crow"
-            // <i class="fas fa-crow"></i>
-        },
-
-        {
-            name: "dog",
-            prefix: "fas",
-            type: "fa",
-            family: "dog"
-            // <i class="fas fa-dog"></i>
-        },
-
-        {
-            name: "dove",
-            prefix: "fas",
-            type: "fa",
-            family: "dove"
-            // <i class="fas fa-dove"></i>
-        },
-
-        {
-            name: "dragon",
-            prefix: "fas",
-            type: "fa",
-            family: "dragon"
-            // <i class="fas fa-dragon"></i>
-        },
-
-        {
-            name: "horse",
-            prefix: "fas",
-            type: "fa",
-            family: "horse"
-            // <i class="fas fa-horse"></i>
-        },
-
-        {
-            name: "hippo",
-            prefix: "fas",
-            type: "fa",
-            family: "hippo"
-            // <i class="fas fa-hippo"></i>
-        },
-
-        {
-            name: "fish",
-            prefix: "fas",
-            type: "fa",
-            family: "fish"
-            // <i class="fas fa-fish"></i>
+    class icone {
+        constructor(name, prefix, type, family){
+            this.name = name;
+            this.prefix = prefix;
+            this.type = type;
+            this.family = family;
         }
-    ]
+    };
+    
+    const cat = new icone("cat", "fas", "fa", "animal");
+    const crow = new icone("crow", "fas", "fa", "animal");
+    const dog = new icone("dog", "fas", "fa", "animal");
+    const dove = new icone("dove", "fas", "fa", "animal");
+    const dragon = new icone("dragon", "fas", "fa", "animal");
+    const horse = new icone("horse", "fas", "fa", "animal");
+    const hippo = new icone("hippo", "fas", "fa", "animal");
+    const fish = new icone("fish", "fas", "fa", "animal");
+    const carrot = new icone("carrot", "fas", "fa", "vegetables");
+    const apple = new icone("apple-alt", "fas", "fa", "vegetables");
+    const lemon = new icone("lemon", "fas", "fa", "vegetables");
+    const pepper = new icone("pepper-hot", "fas", "fa", "vegetables");
+    const astronaut = new icone("user-astronaut", "fas", "fa", "user");
+    const graduate = new icone("user-graduate", "fas", "fa", "user");
+    const ninja = new icone("user-ninja", "fas", "fa", "user");
+    const secret = new icone("user-secret", "fas", "fa", "user");
+    
+    let arrayIcone = [
+        cat, crow, dog, dove, dragon, horse, hippo, fish, carrot, apple, lemon, pepper, astronaut, graduate, ninja, secret
+    ];
    
     // colori come da milestone 2
-    const color = ["blue", "orange", "purple"]
+    const color = ["blue", "orange", "purple"];
    
     //aggiungiamo dei colori come da milestone 2
-    icone.forEach(element => {
-        element.color = color[Math.floor(Math.random()*3)]
+    arrayIcone.forEach(element => {
+        if (element.family === "animal"){
+            element.color = color[0];
+        }else if (element.family === "vegetables"){
+            element.color = color[1];
+        }else if (element.family === "user"){
+            element.color = color[2];
+        }
     });
-    console.log(icone);
     //inseriamo le icone nel container come da milestone 2
-    const icons = $(".icons");
-    let icona = "";
-    icone.forEach(element => {
-        icona = `<div><i class = "${element.prefix} ${element.type}-${element.family}" style ="color:${element.color}"></i></i><div class="title">${element.name}</div></div>`;
-        icons.append(icona);
+    const container = $(".icons");
+    arrayIcone.forEach(element => {
+        let markup = `<div><i class = "${element.prefix} ${element.type}-${element.name}" style ="color:${element.color}"></i></i><div class="title">${element.name}</div></div>`;
+        container.append(markup);
     });
     //estrapoliamo i tipi di icone
-    let [blue, orange, purple] = [color[0], color[1], color[2]];
+    const [blue, orange, purple] = color;
     //aggiungiamo i tipi alla select
-    let select = $("select");
-    let opzioni = `<option value="blue">${blue}</option>
+    const select = $("select");
+    const opzioni = `<option value="blue">${blue}</option>
     <option value="orange">${orange}</option>
     <option value="purple">${purple}</option>`;
     select.append(opzioni);
     //al change mostriamo solo le icone filtrate
     select.change(function(){
         $(".icons div").remove();
-        let newIcone = [];
+        const newIcone = [];
         let x = select.val();
-        icone.forEach(element => {
+        arrayIcone.forEach(element => {
            if (element.color == x){
                newIcone.push(element);
            }else if (x == "all"){
                newIcone.push(element);
            }
         });
-        console.log(newIcone);
-        let newIcona = "";
         newIcone.forEach(element => {
-        newIcona = `<div><i class = "${element.prefix} ${element.type}-${element.family}" style ="color:${element.color}"></i></i><div class="title">${element.name}</div></div>`;
-        icons.append(newIcona);
+        let newmarkup = `<div><i class = "${element.prefix} ${element.type}-${element.name}" style ="color:${element.color}"></i></i><div class="title">${element.name}</div></div>`;
+        container.append(newmarkup);
         });
     });
 });
